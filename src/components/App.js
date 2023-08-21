d8fce0562d29b3b3039ae4a0b02d4083799d03c0
BerkeAnna <berke.anna0803@gmail.com>
2023-08-21 21:42:46 +0200

    most végre működik :D

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,14 @@ import Main from './Main'
 
 class App extends Component {
 
-  state = {
+  /*state = {
     account: null // Itt tároljuk a fiókot
   };
 
   setAccount = (account) => {
     this.setState({ account });
   }
+  */
 
   async componentWillMount() {
     await this.loadWeb3()
@@ -42,9 +43,12 @@ class App extends Component {
       const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
       this.setState({ marketplace })
       const productCount = await marketplace.methods.productCount().call()
+      //console.log("mets " + marketplace.methods)
+      //console.log("prodc:" + marketplace.methods.productCount())
+      console.log("productCount: " + productCount )
       this.setState({ productCount })
 
-      for(var i = 0 ; i<= productCount; i++){
+      for(var i = 1 ; i<= productCount; i++){
         const product = await marketplace.methods.products(i).call()
         this.setState({
           products: [...this.state.products, product]
@@ -52,8 +56,6 @@ class App extends Component {
       }
       this.setState({ loading: false })
       console.log(this.state.products)
-      //console.log(productCount.toString())
-      this.setState({ loading: false})
     } else {
       window.alert('Marketplace contract not deployed to detected network.')
     }
@@ -65,21 +67,30 @@ class App extends Component {
     this.state = {
       account: '',
       productCount: 0,
-      products: [],
-      loading: true
-    }
+      products: [], // Inicializáld a products állapotot üres tömbként
+      loading: true // Inicializáld a loading állapotot true-ként
+    };
 
     this.createProduct = this.createProduct.bind(this)
   }
 
   createProduct(name, price) {
-    this.setState({ loading: true })
+  this.setState({ loading: true }, () => {
+    console.log("elotte");
+    console.log(this.state.marketplace.methods.createProduct(name, price));
+    
     this.state.marketplace.methods.createProduct(name, price).send({ from: this.state.account })
-    .once('receipt', (receipt) => {
-      this.setState({ loading: false })
-    })
-    console.log(this.createProduct.price)
-  }
+    //console.log("ha ez megy, a once meg comment ezt kiírja")
+      .once('receipt', (receipt) => {
+        this.setState({ loading: false }, () => {
+          console.log("utana: " + this.state.marketplace.methods.createProduct);
+          console.log("price: " + price);
+        });
+      });
+  });
+}
+
+  
 
   render() {
     return (
@@ -89,7 +100,7 @@ class App extends Component {
             <main role="main" className="col-lg-12 d-flex">
               { this.state.loading
                 ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
-                : <Main createProduct={this.createProduct} />
+                : <Main products={this.state.products} createProduct={this.createProduct} />
               }
             </main>
           </div>
